d0838f38c7fdc92ef401cc63b325fad80f57c04b
Rajat Sharan <errajatsharan@gmail.com>
2025-06-29 20:50:00 +0530

    feat: Add Day 5 - Exception handling concepts with examples and notes

---
+++ 04-exception-handling/05-error-types.js
@@ -0,0 +1,6 @@
+console.log(new Error("Generic error"));
+console.log(new TypeError("Wrong type"));
+console.log(new ReferenceError("Missing reference"));
+console.log(new SyntaxError("Broken syntax"));
+console.log(new RangeError("Out of range"));
+console.log(new EvalError("Eval not allowed"));
